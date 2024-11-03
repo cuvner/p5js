a20db3ecf247fd98ce590430f0f0ba3398b58e94
cuvner <chriscourage699@hotmail.com>
2024-11-03 13:38:03 +0000

    Update sketch.js

--- hackathon-ar/js/sketch.js
+++ hackathon-ar/js/sketch.js
@@ -4,6 +4,7 @@ function setup() {
 	createCanvas(895, 1280, document.getElementById('canvas-ar')) // poster aspect
 	pixelDensity(1) // prevent 200+ PPI lag
 	colorMode(HSL)
+	background(255)
 }
 
 function draw() {
